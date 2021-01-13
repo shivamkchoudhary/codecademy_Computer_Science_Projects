de6df4ca0c82133cda9fd7c9a4aaf979569d791e
Shivam Choudhary <45448915+shivamkchoudhary@users.noreply.github.com>
2021-01-13 14:37:32 +0530

    Create initials.py

---
+++ Hello World/initials.py
@@ -0,0 +1,8 @@
+# Fun Fact: It's me. 
+print("  SSS   K     K")
+print(" S   S  K    K")
+print(" S      K   K")
+print("  SSS   K K")
+print("     S  K  K")
+print(" S   S  K   K")
+print("  SSS   K     K")
